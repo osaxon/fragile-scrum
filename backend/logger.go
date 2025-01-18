4e56c970f9c835aae3b93e538165f2e76a89834a
Sergei Petrov <s.petrov@live.com>
2025-01-18 19:57:55 +0200

    update dependencies

--- backend/logger.go
+++ backend/logger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -12,7 +13,7 @@ func (app *application) disableHealthRouteLogging() {
 	app.pb.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		se.Router.BindFunc(func(re *core.RequestEvent) error {
 			if re.Request.URL.Path == "/api/health" && re.Request.UserAgent() == "Wget" {
-				re.Set("__skipSuccessActivityLogger", true)
+				return apis.SkipSuccessActivityLog().Func(re)
 			}
 			return re.Next()
 		})
